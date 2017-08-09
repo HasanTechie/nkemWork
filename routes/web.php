7bfeb32ce17552fcd8769e22e9b9db786bf15596
hasanabbax <hasanabbax@gmail.com>
2017-08-09 15:54:23 +0000

    minor 854

--- routes/web.php
+++ routes/web.php
@@ -31,4 +31,4 @@ Route::get('/addproperty', function () {   return view('addproperty');   });
 
 Auth::routes();
 
-//Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/admin', 'AdminController@index')->name('admin');
