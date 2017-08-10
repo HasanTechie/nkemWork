4d55064273703874a8b0dca9af59c3aea4dcab90
hasanabbax <hasanabbax@gmail.com>
2017-08-10 04:27:09 +0000

    minor edit 927

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -74,41 +74,47 @@
                 <div class="filter-list row clearfix">
                 @foreach($properties->slice(0, 9) as $property)
                     @if($imageArray = explode(",", $property->images))
-                    <!--Property Box Two-->
-                        <div class="property-box-two mix all for-{{$property->type}} col-lg-4 col-md-6 col-sm-6 col-xs-12">
-                            <div class="inner-box">
-                                <div class="image-box">
-                                    <figure class="image"><a href="{{$property->path()}}"><img
-                                                    style="display: block; max-width: 360px; min-width: 360px; max-height: 240px; min-height: 240px;"
-                                                    title="{{$property->title}}"
-                                                    src="{{Storage::url($imageArray[0] )}}" alt=""></a>
-                                    </figure>
-                                    <div class="prop-cat {{$property->type}}-cat">{{$property->type}}</div>
-                                </div>
-                                <div class="lower-content">
-                                    <div class="property-title">
-                                        <h3><a href="{{$property->path()}}">{{$property->address}}</a></h3>
-                                        <div class="location"><span class="fa fa-map-marker"></span>&nbsp; {{$property->postcode}} {{$property->city}} {{$property->state}}
+                        <!--Property Box Two-->
+                            <div class="property-box-two mix all for-{{$property->type}} col-lg-4 col-md-6 col-sm-6 col-xs-12">
+                                <div class="inner-box">
+                                    <div class="image-box">
+                                        <figure class="image"><a href="{{$property->path()}}"><img
+                                                        style="display: block; max-width: 360px; min-width: 360px; max-height: 240px; min-height: 240px;"
+                                                        title="{{$property->title}}"
+                                                        src="{{Storage::url($imageArray[0] )}}" alt=""></a>
+                                        </figure>
+                                        <div class="prop-cat {{$property->type}}-cat">{{$property->type}}</div>
+                                    </div>
+                                    <div class="lower-content">
+                                        <div class="property-title">
+                                            <h3><a href="{{$property->path()}}">{{$property->address}}</a></h3>
+                                            <div class="location"><span
+                                                        class="fa fa-map-marker"></span>&nbsp; {{$property->postcode}} {{$property->city}} {{$property->state}}
+                                            </div>
+                                        </div>
+                                        <div class="prop-info">
+                                            <ul class="clearfix">
+                                                <li><strong>{{$property->bedroom}}</strong> Beds</li>
+                                                <li><strong>{{$property->bathroom}}</strong> Baths</li>
+                                                <li><strong>{{$property->area}}</strong> sq ft.</li>
+                                            </ul>
                                         </div>
                                     </div>
-                                    <div class="prop-info">
-                                        <ul class="clearfix">
-                                            <li><strong>{{$property->bedroom}}</strong> Beds</li>
-                                            <li><strong>{{$property->bathroom}}</strong> Baths</li>
-                                            <li><strong>{{$property->area}}</strong> sq ft.</li>
-                                        </ul>
+                                    <div class="bottom-content">
+                                        <div class="price-discount clearfix">
+                                            <div class="price"><strong>&pound;{{$property->price}}</strong>Asking Price
+                                            </div>
+                                            <div class="discount">
+                                                <strong>{{$property->bathroom+5 /*random for testing*/}}%</strong>Bellow
+                                                Market Value
+                                            </div>
+                                        </div>
+                                        <div class="link"><a href="{{$property->path()}}">View Details <span
+                                                        class="fa fa-angle-right"></span></a></div>
                                     </div>
-                                </div>
-                                <div class="bottom-content">
-                                    <div class="price-discount clearfix">
-                                        <div class="price"><strong>&pound;{{$property->price}}</strong>Asking Price</div>
-                                        <div class="discount"><strong>{{$property->bathroom+5 /*random for testing*/}}%</strong>Bellow Market Value</div>
-                                    </div>
-                                    <div class="link"><a href="{{$property->path()}}">View Details <span
-                                                    class="fa fa-angle-right"></span></a></div>
                                 </div>
                             </div>
-                        </div>
+                        @endif
                     @endforeach
 
 
@@ -237,9 +243,10 @@
                                 <div class="clearfix">
                                     <!--Image Column-->
                                     <div class="image-column col-md-3 col-sm-12 col-xs-12">
-                                        <figure class="image"><img style="display: block; max-width: 570px; min-width: 570px; max-height: 503px; min-height: 503px;"
-                                                                   src="{{Storage::url($testimonial->media)}}"
-                                                                   alt="">
+                                        <figure class="image"><img
+                                                    style="display: block; max-width: 570px; min-width: 570px; max-height: 503px; min-height: 503px;"
+                                                    src="{{Storage::url($testimonial->media)}}"
+                                                    alt="">
                                         </figure>
                                     </div>
                                     <!--Content Column-->
